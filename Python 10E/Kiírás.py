f9a8782702959ac7b9e0ce8dc88fd63b68268ade
marcellkoti <162977776+marcellkoti@users.noreply.github.com>
2026-02-13 12:07:55 +0100

    Add files via upload

---
+++ Python 10E/Kiírás.py
@@ -0,0 +1,14 @@
+print(5+6)
+print(11*324)
+print("Koteles","Marcell","IKT \"projekt\" ",sep=" ", end=";")
+print("23 és 34 összege ", 23+34)
+print("11 és 22 szorzata\t ", 11+22, "\n ennek harmada:\t",33/3)
+print("----LITERÁLOK----")
+we_a=3
+print(we_a)
+we_b=10
+print(we_a+we_b,we_a*we_b,we_a/we_b,sep=" ")
+we_a='a'
+print(type(we_a))
+#print(we_a+we_b,we_a*we_b,we_a/we_b,sep=" ")
+
---
+++ Python 10E/python.py
@@ -0,0 +1,41 @@
+Python 3.11.7 (tags/v3.11.7:fa7a6f2, Dec  4 2023, 19:24:49) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======================= RESTART: Z:/Python 10E/Kiírás.py =======================
+11
+3564
+Koteles Marcell IKT "projekt" ;23 és 34 összege  57
+11 és 22 szorzata	  33 
+ ennek harmada:	 11.0
+----LITERÁLOK----
+3
+13 30 0.3
+Traceback (most recent call last):
+  File "Z:/Python 10E/Kiírás.py", line 12, in <module>
+    print(we_a+we_b,we_a*we_b,we_a/we_b,sep=" ")
+TypeError: can only concatenate str (not "int") to str
+>>> 
+======================= RESTART: Z:/Python 10E/Kiírás.py =======================
+11
+3564
+Koteles Marcell IKT "projekt" ;23 és 34 összege  57
+11 és 22 szorzata	  33 
+ ennek harmada:	 11.0
+----LITERÁLOK----
+3
+13 30 0.3
+Traceback (most recent call last):
+  File "Z:/Python 10E/Kiírás.py", line 12, in <module>
+    print(we_a+we_b,we_a*we_b,we_a/we_b,sep=" ")
+TypeError: can only concatenate str (not "int") to str
+>>> 
+======================= RESTART: Z:/Python 10E/Kiírás.py =======================
+11
+3564
+Koteles Marcell IKT "projekt" ;23 és 34 összege  57
+11 és 22 szorzata	  33 
+ ennek harmada:	 11.0
+----LITERÁLOK----
+3
+13 30 0.3
+<class 'str'>
